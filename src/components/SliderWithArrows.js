c06f106cb04f087489bc5ef78499857c9df99577
whatislove12 <vlad_podymskiy@mail.ru>
2023-08-06 23:36:29 +0200

    Add loader

---
+++ src/components/SliderWithArrows.js
@@ -0,0 +1,8 @@
+export const SliderWithArrows = ({children}) => {
+
+    return (
+        <div className="w-full h-full flex justify-center items-center gap-3">
+            {children}
+        </div>
+    )
+}
--- src/request/images.js
+++ src/request/images.js
@@ -1,11 +1,11 @@
 export const getImages = async () => {
-  const limitImages = 30;
+  const limitImages = 100;
   return fetch(
     `https://naruto-api.fly.dev/api/v1/characters?limit=${limitImages}`
   )
     .then((response) => response.json())
     .then((json) => {
-      const dateFilter = json.map(item => ({name: item.name, images: item.images}))
+      const dateFilter = json.map(item => ({name: item.name, images: item.images[0]}))
       return dateFilter
     })
     .catch((error) => {
